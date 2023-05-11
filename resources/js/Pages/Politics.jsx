34a05b1bc692f4d5f594ec4dc632e431ac63223d
{Niiii} <tapioka2real@gmail.com>
2023-05-11 09:34:19 +0900

    politics seisakpolitics seisakuu

---
+++ resources/js/Pages/Politics.jsx
@@ -0,0 +1,8 @@
+export default function Politics() {
+    return (
+        <div>
+            <div className="politics_table">政党早見表</div>
+        </div>
+    );
+}
+
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('hello-world');
+    return Inertia::render('Politics');
 });
