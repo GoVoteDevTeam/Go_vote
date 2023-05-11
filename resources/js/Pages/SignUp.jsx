0e8f030f08e591ffbbe411e8c24a557433c833eb
naruto1031 <naruto0614@au.com>
2023-05-11 12:33:48 +0900

    create page

---
+++ resources/js/Pages/SignUp.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const SignUp = () => {
+    return (
+        <>
+            <SignUPPage>
+                signup
+            </SignUPPage>
+        </>
+    )
+}
+
+export default SignUp;
+
+const SignUPPage = styled.div`
+
+`;
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/', function () {
 Route::get('/demo', function () {
     return Inertia::render('Demo');
 });
+
+
+Route::get('/signup', function () {
+    return Inertia::render('SignUp');
+});
