5bd24324bc19457c4db990f58c285ec0582554fe
reiya0208 <yukimura0208ta@gmail.com>
2023-05-26 13:50:25 +0900

    白丸追加

--- resources/js/Pages/hello-world.jsx
+++ resources/js/Pages/hello-world.jsx
@@ -1,12 +1,20 @@
+import styled from "styled-components";
 import Header from "../components/Header"
 
 export default function HelloWorld() {
     return (
         <>
             <Header />
+            <BodyElement>
             <div>
                 <div className="testCakse"></div>
             </div>
+            </BodyElement>
         </>
     );
 }
+
+const BodyElement = styled.body`
+    width: 100%;
+    height: 700px;
+`
--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -4,8 +4,13 @@ import logo from "../../../public/logo.png"
 
 const Header = () => {
     return(
+    
+
         <HeaderElement>
           <img className="logo" src={logo}/>
+
+          <div className="icon"></div>
+          
         </HeaderElement>
     )
 }
@@ -16,11 +21,25 @@ const HeaderElement = styled.header`
         width: 100%;
         height: 100px;
         text-align: center;
+        align-items: center;
         margin: 0;
         padding: 0;
         background-color: #36375f;
+        display: flex;
         .logo {
           height: 100px;
+          margin-left: 45%;
+        }
+        .icon {
+          top: 20px;
+          right: 20px;
+          width: 50px;
+          height: 50px;
+          margin: 0;
+          margin-left: 32%;
+          padding: 0;
+          border-radius: 50%;
+          background-color: white;
         }
         
 `;
