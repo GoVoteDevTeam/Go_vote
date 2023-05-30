cc7f5ea1a874fafa4c38361002bb0c8140479987
duwns5769 <duwns5769@gmail.com>
2023-05-30 23:13:28 +0900

    ok

--- resources/js/Pages/News.jsx
+++ resources/js/Pages/News.jsx
@@ -1,42 +1,68 @@
-import { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
 
-const news = () => {    
+const Tabs = () => {
+
+    const [currentTab, setCurrentTab] = useState('1');
+    const tabs = [
+        {
+            id: 1,
+            tabTitle: 'Tab 1',
+            title: 'Title 1',
+            content: 'Las tabs se generan automáticamente a partir de un array de objetos, el cual tiene las propiedades: id, tabTitle, title y content.'
+        },
+        {
+            id: 2,
+            tabTitle: 'Tab 2',
+            title: 'Title 2',
+            content: 'Contenido de tab 2.'
+        },
+        {
+            id: 3,
+            tabTitle: 'Tab 3',
+            title: 'Title 3',
+            content: 'Contenido de tab 3.'
+        },
+        {
+            id: 4,
+            tabTitle: 'Tab 4',
+            title: 'Title 4',
+            content: 'Contenido de tab 4.'
+        }
+    ];
+
+    const handleTabClick = (e) => {
+        setCurrentTab(e.target.id);
+    }
+
     return (
-        <>
-            <HeaderElement>
-
-            </HeaderElement>
-            <News_Page>
-                <div className="title">
-                    ニュース
-                </div>
-
-                <div className="newspage">
-                function TabMenu({ tabs, activeTab, onChange }) {
-    return (
-    <ul>
-      {tabs.map((tab) => (
-        <li
-          key={tab}
-          className={tab === activeTab ? "active" : ""}
-          onClick={() => onChange(tab)}
-        >
-          {tab}
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-                </div>
+      <>
+        <HeaderElement>
+          <News_Page>
+            <div className="title">
+              ニュース
+            </div>
             </News_Page>
-        </>
-    )
+        </HeaderElement>
 
+        <div className='container'>
+            <div className='tabs'>
+                {tabs.map((tab, i) =>
+                    <button key={i} id={tab.id} disabled={currentTab === `${tab.id}`} onClick={(handleTabClick)}>{tab.tabTitle}</button>
+                )}
+            </div>
+            <div className='content'>
+                {tabs.map((tab, i) =>
+                    <div key={i}>
+                        {currentTab === `${tab.id}` && <div><p className='title'>{tab.title}</p><p>{tab.content}</p></div>}
+                    </div>
+                )}
+            </div>
+        </div>
+      </>
+    );
 }
 
-export default news;
+export default Tabs;
 
 const HeaderElement = styled.header`
     width: 100%;
