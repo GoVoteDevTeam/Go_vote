d1fbbe685f54840e994af27a8f6fb7aeb58dce3b
reiya0208 <yukimura0208ta@gmail.com>
2023-06-01 10:18:49 +0900

    インデント調整

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -21,35 +21,35 @@ const Header = () => {
 export default Header;
 
 const HeaderElement = styled.header`
-      width: 100%;
-      height: 100px;
-      position: relative;
-      text-align: center;
-      align-items: center;
-      margin: 0;
-      padding: 0;
-      background-color: #36375f;
-      display: flex;
-      .logo-container{
-        width: 100%;
-        text-align: center;
-        position: absolute;
-      }
-      .logo {
-        height: 100px;
-      }
-      .icon-container{
-        width: 100%;
-        position: absolute;
-      }
-      .icon {
-        float: right;
-        width: 50px;
-        height: 50px;
-        margin-right: 15px;
-        padding: 0;
-        border-radius: 50%;
-        background-color: white;
-      }
-        
+  width: 100%;
+  height: 100px;
+  position: relative;
+  text-align: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  background-color: #36375f;
+  display: flex;
+  .logo-container{
+    width: 100%;
+    text-align: center;
+    position: absolute;
+  }
+  .logo {
+    height: 100px;
+  }
+  .icon-container{
+    width: 100%;
+    position: absolute;
+  }
+  .icon {
+    float: right;
+    width: 50px;
+    height: 50px;
+    margin-right: 15px;
+    padding: 0;
+    border-radius: 50%;
+    background-color: white;
+  }
+    
 `;
