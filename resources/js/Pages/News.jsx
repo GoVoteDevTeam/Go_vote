c7c4fa64bde9edf2755e0583978e26bb8c4e742b
naruto1031 <naruto0614@au.com>
2023-06-03 20:42:08 +0900

    axiosのコメントアウト解除

--- resources/js/Pages/News.jsx
+++ resources/js/Pages/News.jsx
@@ -41,10 +41,10 @@ const News = () => {
     }
 
     useEffect(() => {
-		// const newsContents = axios.get(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${import.meta.env.VITE_NEWS_KEY}`).then((res) => {
-		// 	console.log(res.data.articles);
-		// });
-        // console.log(newsContents);
+		const newsContents = axios.get(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${import.meta.env.VITE_NEWS_KEY}`).then((res) => {
+			console.log(res.data.articles);
+		});
+        console.log(newsContents);
 	})
 
     return (
