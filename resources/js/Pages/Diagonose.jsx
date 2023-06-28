f43a30c17d5e2191deb053764db70058298230b6
shunyadesu <nysj1210@gmail.com>
2023-06-28 09:59:32 +0900

    ひかるくんと共有

--- resources/js/Pages/Diagonose.jsx
+++ resources/js/Pages/Diagonose.jsx
@@ -5,42 +5,55 @@ import { useState } from "react";
 import Footer from "../components/footer";
 import VoteNotice from '../components/VoteNotice';
 
+var variables = {
+  answer1: 0,
+  answer2: 0,
+  answer3: 0,
+  answer4: 0,
+  answer5: 0,
+  answer6: 0,
+  answer7: 0,
+  answer8: 0,
+  answer9: 0,
+  answer: 0,
+};
+
 const questions = [
   {
-    number: "1",
-    title: "経済",
-    question: "国民への給付金に対してどう思うか？",
-    answer1: "国民一律に必要である。",
-    answer2: "部分的に必要である。",
-    answer3: "不要である",
-    answer4: "特になし"
+      number: "1",
+      title: "経済",
+      question: "国民への給付金に対してどう思うか？",
+      answer1: "国民一律に必要である。",
+      answer2: "部分的に必要である。",
+      answer3: "不要である",
+      answer4: "特になし"
   },
   {
-    number: "2",
-    title: "財政・税制",
-    question: "財政・税制に対してどう思うか？",
-    answer1: "PB黒字化破壊",
-    answer2: "PB黒字化見直し",
-    answer3: "PB黒字化維持",
-    answer4: "特になし"
+      number: "2",     //政党名
+      title: "財政・税制",       //キャッチコピー
+      question: "財政・税制に対してどう思うか？", //詳細
+      answer1: "PB黒字化破壊",
+      answer2: "PB黒字化見直し",
+      answer3: "PB黒字化維持",
+      answer4: "特になし"
   },
   {
-    number: "3",
-    title: "子育て・教育",
-    question: "幼児教育の無償化",
+    number: "3",     //政党名
+    title: "子育て・教育",       //キャッチコピー
+    question: "幼児教育の無償化", //詳細
     answer1: "完全無償化",
     answer2: "段階的無償化",
     answer3: "条件付き無償化",
     answer4: "特になし"
   },
   {
-    number: "4",
-    title: "日米同盟",
-    question: "日米関係についてどう思うか？",
+    number: "4",     //政党名
+    title: "日米同盟",       //キャッチコピー
+    question: "日米関係についてどう思うか？",      //詳細
     answer1: "対米自立",
     answer2: "同盟維持",
     answer3: "PB黒字化維持",
-    answer4: "特になし"
+    answer4: "特になし"     
   },
 ];
 
@@ -49,22 +62,24 @@ const Diagnose = () => {
     console.log(data.name);
   });
 
-  const Politicses = () => {
-    return data.map((data) => {
-      return <div className="">{data.name}</div>;
-    });
-  };
-
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedOption, setSelectedOption] = useState("");
+  const [error, setError] = useState(false);
 
   const handleNextQuestion = () => {
+    if (selectedOption === "") {
+      setError(true); // オプションが選択されていない場合にエラーを設定
+      return;
+    }
+
     setSelectedOption("");
     setCurrentQuestion(currentQuestion + 1);
+    setError(false); // エラーをリセット
   };
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
+    setError(false); // オプションが選択されたらエラーをリセット
   };
 
   return (
@@ -112,6 +127,17 @@ const Diagnose = () => {
                   />
                   {questions[currentQuestion].answer3}
                 </p>
+                <p>
+                  <input
+                    type="radio"
+                    name="option"
+                    value="option4"
+                    checked={selectedOption === "option4"}
+                    onChange={handleOptionChange}
+                  />
+                  {questions[currentQuestion].answer4}
+                </p>
+                {error && <p>一つ選んで下さい。</p>} {/* エラーメッセージの表示 */}
                 <button onClick={() => handleNextQuestion()}>次の問題へ</button>
               </>
             ) : (
